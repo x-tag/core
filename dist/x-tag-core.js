2c13d06043a2f342e827f575e9b7b801af2d3e55
Daniel Buchner <danieljb2@gmail.com>
2013-09-24 15:47:53 -0700

    updated dist with TouchEvent proto fix

--- dist/x-tag-core.js
+++ dist/x-tag-core.js
@@ -2949,10 +2949,7 @@ var touchReset = {
   };
 
 if (win.TouchEvent) {
-  for (z in TouchEventProto) {
-    win.TouchEvent.prototype[z] = TouchEventProto[z];
-    Object.defineProperty(win.TouchEvent.prototype, z, TouchEventProto[z]);
-  }
+  for (z in TouchEventProto) win.TouchEvent.prototype[z] = TouchEventProto[z];
 }
 
 /*** Custom Event Definitions ***/
