22eabc4e0d605b6c02b7fd011fbd546905bd07d0
Daniel Buchner <danieljb2@gmail.com>
2017-03-07 08:09:42 -0800

    draft update

--- src/core-v2.js
+++ src/core-v2.js
@@ -2,23 +2,36 @@
 
 (function(){
 
-  function delegateAction(pseudo, event) {
-    var match,
-        target = event.target,
-        root = event.currentTarget;
-    while (!match && target && target != root) {
-      if (target.tagName && matchSelector.call(target, pseudo.value)) match = target;
-      target = target.parentNode;
-    }
-    if (!match && root.tagName && matchSelector.call(root, pseudo.value)) match = root;
-    return match ? pseudo.listener = pseudo.listener.bind(match) : null;
-  }
+  // var root = document.documentElement;
+  // var matches = function(node, selector) {
+  //   return this.call(node, selector);
+  // }.bind(root.matches || root.webkitMatchesSelector || root.mozMatchesSelector || root.msMatchesSelector || root.oMatchesSelector);
+
+  // NodeList.prototype.forEach = NodeList.prototype.forEach || Array.prototype.forEach;
+
+  // function delegateAction(pseudo, event) {
+  //   var match,
+  //       target = event.target,
+  //       root = event.currentTarget;
+  //   while (!match && target && target != root) {
+  //     if (target.tagName && matches(target, pseudo.value)) match = target;
+  //     target = target.parentNode;
+  //   }
+  //   if (!match && root.tagName && matches(root, pseudo.value)) match = root;
+  //   return match ? pseudo.listener = pseudo.listener.bind(match) : null;
+  //}
+
+  var regexReplaceCommas = /,/g;
+  var regexCamelToDash = /([a-z])([A-Z])/g;
+  var regexPseudoParens = /\(|\)/g;
+  var regexPseudoCapture = /:(\w+)\u276A(.+?(?=\u276B))|:(\w+)/g;
+  var regexDigits = /(\d+)/g;
 
   xtag = {
     pseudos: {
-      delegate: {
-        action: delegateAction
-      },
+      // delegate: {
+      //   action: delegateAction
+      // },
       preventable: {
         action: function (pseudo, event) {
           return !event.defaultPrevented;
@@ -40,20 +53,21 @@
     },
     mixins: {},
     extensions: {
-      native: {
-        onImplement: function(){}
-      },
       reactions: {
 
       },
       methods: {
-
+        onDeclare: function(base, methods){
+          console.log(methods)
+        }
       },
       accessors: {
 
       },
       events: {
-
+        onConnect: function(){
+          console.log('event added')
+        }
       },
       pseudos: {
 
@@ -63,43 +77,98 @@
       }
     },
     define: function (name, def) {
-      //customElements.define(name, def);
+      // customElements.define(name, def);
+    },
+    applyPseudos: function(key, fn, target, source) {
+      var listener = fn,
+          pseudos = {};
+      if (key.match(':')) {
+        var matches = [],
+            valueFlag = 0;
+        key.replace(regexPseudoParens, function(match){
+          if (match == '(') return ++valueFlag == 1 ? '\u276A' : '(';
+          return !--valueFlag ? '\u276B' : ')';
+        }).replace(regexPseudoCapture, function(z, name, value, solo){
+          matches.push([name || solo, value]);
+        });
+        var i = matches.length;
+        while (i--) parsePseudo(function(){
+          var name = matches[i][0],
+              value = matches[i][1];
+          if (!xtag.pseudos[name]) throw "pseudo not found: " + name + " " + value;
+          value = (value === '' || typeof value == 'undefined') ? null : value;
+          var pseudo = pseudos[i] = Object.create(xtag.pseudos[name]);
+          pseudo.key = key;
+          pseudo.name = name;
+          pseudo.value = value;
+          pseudo['arguments'] = (value || '').split(',');
+          pseudo.action = pseudo.action || trueop;
+          pseudo.source = source;
+          pseudo.onAdd = pseudo.onAdd || noop;
+          pseudo.onRemove = pseudo.onRemove || noop;
+          var original = pseudo.listener = listener;
+          listener = function(){
+            var output = pseudo.action.apply(this, [pseudo].concat(toArray(arguments)));
+            if (output === null || output === false) return output;
+            output = pseudo.listener.apply(this, arguments);
+            pseudo.listener = original;
+            return output;
+          };
+          if (!target) pseudo.onAdd.call(fn, pseudo);
+          else target.push(pseudo);
+        });
+      }
+      for (var z in pseudos) {
+        if (pseudos[z].onCompiled) listener = pseudos[z].onCompiled(listener, pseudos[z]) || listener;
+      }
+      return listener;
+    },
+    removePseudos: function(target, pseudos){
+      pseudos.forEach(function(obj){
+        obj.onRemove.call(target, obj);
+      });
+    },
+    fireEvent: function(node, name, obj = {}){
+      node.dispatchEvent(new CustomEvent(name, obj));
     }
   }
 
-  var protos = {}
-
   function createClass(name){
 
-    var _name = name || 'HTMLElement';
-    var base = protos[_name] || (protos[_name] = class extends (name ? Object.getPrototypeOf(document.createElement(name)) : HTMLElement) {
+    var base = class extends (name ? Object.getPrototypeOf(document.createElement(name)).constructor : HTMLElement) {
       constructor () {
         super();
-        //construct(this);
+      }
+      connectedCallback (){
+        processExtensions(base.definition, this, 'onConnect'); 
       }
     });
 
-    base.implements = function(obj){
-      for (let z in obj) {
-        let extension = xtag.extensions[z].onImplement;
-        if (extension) extension(base, obj[z]);
-      }
+    base.definition = {};
+
+    base.with = function(def){ 
+      base = def.native && base == XTagElement ? createClass(def.native) : createClass();
+      base.definition = def;
+      processExtensions(def, base, 'onDeclare');
       return base;
-    }
+    };
 
     return base;
 
   };
 
+  function processExtensions(def, obj, event){
+    for (let z in def) {
+      let extension = xtag.extensions[z];
+      if (extension && extension[event]) extension[event](obj, def[z]);
+    }
+  }
+
   XTagElement = createClass();
 
 })();
 
-// Test
-
-
-
-class Clock extends XTagElement.implements({
+class Clock extends XTagElement.with({
 
   native: 'video',
 
@@ -134,4 +203,4 @@ class Clock extends XTagElement.implements({
 
 }){};
 
-xtag.define('x-clock', Clock);
+//xtag.define('x-clock', Clock);
